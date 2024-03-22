2c6c5751bc77fa207862aa02f9a27034557b051e
Mobina <153940657+MA-0713@users.noreply.github.com>
2024-03-23 00:32:32 +0330

    Add files via upload

---
+++ 001. Indoor.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 17 14:59:20 2024
+
+@author: The Owl
+"""
+#CS P0-1
+#user input
+user_input = input ("Type please:")
+#lower case (lc) 
+lc_input = user_input.lower()
+#output (in lc)
+print (lc_input) 
+        
---
+++ 002. Playback Speed.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 23 00:06:02 2024
+
+@author: The Owl
+"""
+
+#CS50 P01 - 2
+#ask for input:
+user_input2 = input ("Enter your sentence: ")
+#Space to "..."
+new_input2 = user_input2.replace(" ", "...")
+#output (with ... instead) 
+print(new_input2) 
